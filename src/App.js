7f5f1e85aaa6acca104330de631b4ee5e24fe7d7
Gowtham <gowthamsgobi@gmail.com>
2023-05-03 14:40:17 +0530

    final commit

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import Home from "./components/home";
 import EmpCreate from "./components/empcreate/index";
 import EmpView from "./components/empview/index";
 import EmpEdit from "./components/empedit/index";
-
 import Page404 from "./components/page404/index";
 
 const App = () => {
@@ -16,7 +15,6 @@ const App = () => {
         <Route path="employee/create" element={<EmpCreate />} />
         <Route path="employee/view/:empId" element={<EmpView />} />
         <Route path="employee/update/:empId" element={<EmpEdit />} />
-       
         <Route path="*" element={<Page404 />} />
       </Routes>
     </>
